4fc9c88922adbca4da4153a8917bc40188a24b5e
Loïc d'Anterroches <titoo@users.sourceforge.net>
2011-03-05 22:10:48 +0100

    Added the datetime tests.

--- src/photon/datetime.php
+++ src/photon/datetime.php
@@ -56,7 +56,7 @@ class Date
     /**
      * Returns a Date object or false on errors.
      *
-     * @param $format String format passed to strptime()
+     * @param $format String format passed to date_parse_from_format()
      * @param $date Date string to be parsed
      * @return mixed Date object or false on errors
      */
@@ -67,17 +67,8 @@ class Date
 
             return false;
         }
-        $defaults = array(
-                          'year' => 0, 'month' => 1, 'day' => 1,
-                          'hour' => 0, 'minute' => 0, 'second' => 0,
-                          'fraction' => 0);
-        $p = array_merge($defaults, $p);
-        if (false !== strpos($format, 'y') && $p['year'] < 100) {
-            // Match with a two digit year format, need to "interpret" it.
-            $p['year'] = (self::$cutoff_year < $p['year'])
-                ? $p['year'] + 1900
-                : $p['year'] + 2000;
-        }
+        $p = array_merge(array('year' => 0, 'month' => 1, 'day' => 1), $p);
+
         return new Date($p['year'], $p['month'], $p['day']);
     }
 
@@ -100,20 +91,6 @@ class Date
     }
 }
 
-
-
-/**
- * Timespan class.
- *
- * The time class is just a time duration. For example, how long does
- * it take to cook an egg. It is not the time of a day, for that, you
- * have the DateTime class.
- */
-class Timespan
-{
-
-}
-
 /**
  * DateTime.
  *
@@ -123,11 +100,6 @@ class Timespan
  */
 class DateTime extends \DateTime
 {
-    /**
-     * Cutoff year to interpret two digit dates.
-     */
-    public static $cutoff_year = 30;
-
     /**
      * Returns a DateTime object or false on errors.
      *
@@ -152,16 +124,14 @@ class DateTime extends \DateTime
                           'hour' => 0, 'minute' => 0, 'second' => 0,
                           'fraction' => 0);
         $p = array_merge($defaults, $p);
-        if (false !== strpos($format, 'y') && $p['year'] < 100) {
-            // Match with a two digit year format, need to "interpret" it.
-            $p['year'] = (self::$cutoff_year < $p['year'])
-                ? $p['year'] + 1900
-                : $p['year'] + 2000;
-        }
         $date = sprintf('%04d-%02d-%02d %02d:%02d:%02d',
                         $p['year'], $p['month'], $p['day'],
                         $p['hour'], $p['minute'], $p['second']);
-        return self::createFromFormat('Y-m-d H:i:s', $date);
+
+        // self::createFromFormat returns a \DateTime object, so we
+        // need to use it to create a \photon\datetime\DateTime
+        // object.
+        return new DateTime(self::createFromFormat('Y-m-d H:i:s', $date)->format('c'));
     }
 
     public function __toString()
@@ -170,182 +140,3 @@ class DateTime extends \DateTime
     }
 }
 
-/**
- * Get a GM Date in the format YYYY-MM-DD HH:MM:SS and returns a
- * string with the given format in the current timezone.
- *
- * @param $gmdate GMT YYYY-MM-DD HH:MM:SS date (usually stored in the db)
- * @param string Format to be given to strftime ('%Y-%m-%d %H:%M:%S')
- * @return string Formated GMDate into the local time
- */
-function gmstrfdate($gmdate, $format='%Y-%m-%d %H:%M:%S')
-{
-    return strftime($format, strtotime($gmdate . 'Z'));
-}
-
-/**
- * Get a GM Date in the format YYYY-MM-DD HH:MM:SS and returns a
- * string with the given format in GMT.
- *
- * @param string GMDate
- * @param string Format to be given to date ('c')
- * @return string Formated GMDate into GMT
- */
-function gmtogmstrfdate($gmdate, $format='c')
-{
-    return date($format, strtotime($gmdate . 'Z'));
-}
-
-/**
- * Day compare.
- *
- * Compare if the first date is before or after the second date.
- * Returns:
- *    0 if the days are the same.
- *    1 if the first date is before the second.
- *   -1 if the first date is after the second.
- *
- * @param string YYYY-MM-DD date.
- * @param string YYYY-MM-DD date (today local time).
- * @return int
- */
-function day_compare($date1, $date2=null)
-{
-    $date2 = (is_null($date2)) ? date('Y-m-d') : $date2;
-    if ($date2 == $date1) return 0;
-    if ($date1 > $date2) return -1;
-    return 1;
-}
-
-/**
- * Compare two date and returns the number of seconds between the
- * first and the second. If only the date is given without time, the
- * end of the day is used (23:59:59).
- *
- * @param string Date to compare for ex: '2006-09-17 18:42:00'
- * @param string Second date to compare if null use now (null)
- * @return int Number of seconds between the two dates. Negative 
- *             value if the second date is before the first. 
- */
-function daytime_compare($date1, $date2=null)
-{
-    if (strlen($date1) == 10) {
-        $date1 .= ' 23:59:59';
-    }
-    if (is_null($date2)) {
-        $date2 = time();
-    } else {
-        if (strlen($date2) == 10) {
-            $date2 .= ' 23:59:59';
-        }
-        $date2 = strtotime(str_replace('-', '/', $date2));
-    }
-    $date1 = strtotime(str_replace('-', '/', $date1));
-    return $date2 - $date1;
-}
-
-/**
- * Display a date in the format:
- * X days Y hours ago
- * X hours Y minutes ago
- * X hours Y minutes left
- *
- * "resolution" is year, month, day, hour, minute.
- *
- * If not time is given, only the day, the end of the day is
- * used: 23:59:59.
- *
- * @param string Date to compare with ex: '2006-09-17 18:42:00'
- * @param string Reference date to compare with by default now (null)
- * @param int Maximum number of elements to show (2)
- * @param string If no delay between the two dates display ('now')
- * @param bool Show ago/left suffix
- * @return string Formatted date
- */
-function easy($date, $ref=null, $blocks=2, $notime='now', $show=true)
-{
-    if (strlen($date) == 10) {
-        $date .= ' 23:59:59';
-    }
-    if (is_null($ref)) {
-        $ref = date('Y-m-d H:i:s'); 
-        $tref = time();
-    } else {
-        if (strlen($ref) == 10) {
-            $ref .= ' 23:59:59';
-        }
-        $tref = strtotime(str_replace('-', '/', $ref));
-    }
-    $tdate = strtotime(str_replace('-', '/', $date));
-    $past = true;
-    if ($tref < $tdate) {
-        // date in the past
-        $past = false;
-        $_tmp = $ref;
-        $ref = $date;
-        $date = $_tmp;
-    }
-    $ref = str_replace(array(' ', ':'), '-', $ref);
-    $date = str_replace(array(' ', ':'), '-', $date);
-    $refs = explode('-', $ref);
-    $dates = explode('-', $date);
-    // Modulo on the month is dynamically calculated after
-    $modulos = array(365, 12, 31, 24, 60, 60);
-    // day in month 
-    $month = $refs[1] - 1;
-    $modulos[2] = date('t', mktime(0, 0, 0, $month, 1, $refs[0]));
-    $diffs = array();
-    for ($i=0; $i<6; $i++) {
-        $diffs[$i] = $refs[$i] - $dates[$i];
-    }
-    $retain = 0;
-    for ($i=5; $i>-1; $i--) {
-        $diffs[$i] = $diffs[$i] - $retain;
-        $retain = 0;
-        if ($diffs[$i] < 0) {
-            $diffs[$i] = $modulos[$i] + $diffs[$i];
-            $retain = 1;
-        }
-    }
-    $res = '';
-    $total = 0;
-    for ($i=0; $i<5; $i++) {
-        if ($diffs[$i] > 0) {
-            $total++;
-            $res .= $diffs[$i].' ';
-            switch ($i) {
-            case 0: 
-                $res .= _n('year', 'years', $diffs[$i]);
-            	break;
-            case 1: 
-                $res .= _n('month', 'months', $diffs[$i]);
-            	break;
-            case 2: 
-                $res .= _n('day', 'days', $diffs[$i]);
-            	break;
-            case 3: 
-                $res .= _n('hour', 'hours', $diffs[$i]);
-            	break;
-            case 4: 
-                $res .= _n('minute', 'minutes', $diffs[$i]);
-            	break;
-            case 5: 
-                $res .= _n('second', 'seconds', $diffs[$i]);
-            	break;
-            }
-            $res .= ' ';
-        }
-        if ($total >= $blocks) break;
-    }
-    if (strlen($res) == 0) {
-        return $notime;
-    }
-    if ($show) {
-        if ($past) {
-            $res = sprintf(__('%s ago'), $res);
-        } else {
-            $res = sprintf(__('%s left'), $res);
-        }
-    }
-    return $res;
-}
