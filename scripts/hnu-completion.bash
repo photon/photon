387f734ac1dd84efa307451ad8d585b2042a0b7c
Mehdi Kabab <pioupioum@gmail.com>
2011-03-09 11:06:18 +0100

    Updated the Bash completion support for Photon.

--- scripts/hnu-completion.bash
+++ scripts/hnu-completion.bash
@@ -1,34 +1,131 @@
 #!bash
-#
-# Bash completion support for Photon.
-#
+# hnu-completion
+# ==============
+# 
+# Bash completion support for [Photon](http://www.photon-project.com/).
+# 
+# Installation
+# ------------
+# 
+# The easiest way (but not necessarily the cleanest) is to copy it somewhere
+# (e.g. `~/.hnu-completion.sh`) and put the following line in your `.bashrc`:
+# 
+#     source ~/.hnu-completion.sh
+# 
+# Otherwise, the most comprehensive methodology is as follows:
+# 
+# 1. If you have not already done:
+# 
+#    1. Create the directory `~/bash_completion.d`.
+# 
+#    2. Put the following lines in your `.bashrc`:
+# 
+#       ~~~
+#       export USER_BASH_COMPLETION_DIR=~/bash_completion.d
+#       if [ -f /usr/local/etc/bash-completion.d ]; then
+#         . /usr/local/etc/bash-completion.d
+#       fi
+#       ~~~
+# 
+#       Note: the bash_completion script can be at a different location depending on your system, like:
+# 
+#         * `/usr/local/etc/bash-completion.d`
+#         * `/opt/local/etc/bash-completion.d` (with macports)
+# 
+#    3. Put in the ~/.bash-completion file the following code:
+# 
+#       ~~~
+#       # source user completion directory definitions
+#       if [[ -d $USER_BASH_COMPLETION_DIR && -r $USER_BASH_COMPLETION_DIR && \
+#             -x $USER_BASH_COMPLETION_DIR ]]; then
+#           for i in $(LC_ALL=C command ls "$USER_BASH_COMPLETION_DIR"); do
+#               i=$USER_BASH_COMPLETION_DIR/$i
+#               [[ ${i##*/} != @(*~|*.bak|*.swp|\#*\#|*.dpkg*|*.rpm@(orig|new|save)|Makefile*) \
+#                  && -f $i && -r $i ]] && . "$i"
+#           done
+#       fi
+#       unset i
+#       ~~~
+# 
+# 2. Copy the hnu-completion file in your ~/bash-completion.d (e.g. `~/bash-completion.d/hnu`).
+# 3. Now, open a new shell window and type `hn<TAB> sel<TAB> --c<TAB>/tmp/photon/`. Enjoy!
+# 
+# LGPL License
+# ------------
+# 
 # This file is part of Photon, the High Speed PHP Framework.
 # Copyright (C) 2011 Loic d'Anterroches and contributors.
-#
+# 
 # Photon is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation; either version 2.1 of the License.
-#
+# 
 # Photon is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# http://www.photon-project.com/
 
 have hnu &&
 {
-_hnu()
-{
-    local cur prev global_opts commands server_commands
 
-    global_opts="--help --version --verbose --conf="
-    commands="init testserver runtests selftest server taskstart secretkey"
-    server_commands="start stop less list childstart"
+# The following function is based on code from:
+# 
+# bash completion support for core Git.
+#
+# Copyright (C) 2006,2007 Shawn O. Pearce <spearce@spearce.org>
+# Conceptually based on gitcompletion (http://gitweb.hawaga.org.uk/).
+# Distributed under the GNU General Public License, version 2.0.
+#
+# __hnucomp_1 requires 2 arguments
+__hnucomp_1 ()
+{
+    local c IFS=' '$'\t'$'\n'
+    for c in $1; do
+        case "$c$2" in
+            --*=*) printf %s$'\n' "$c$2"  ;;
+            *.)    printf %s$'\n' "$c$2"  ;;
+            *)     printf %s$'\n' "$c$2 " ;;
+        esac
+    done
+} # __hnucomp_1 ()
+
+# The following function is based on code from:
+# 
+# bash completion support for core Git.
+#
+# Copyright (C) 2006,2007 Shawn O. Pearce <spearce@spearce.org>
+# Conceptually based on gitcompletion (http://gitweb.hawaga.org.uk/).
+# Distributed under the GNU General Public License, version 2.0.
+#
+# __hnucomp accepts 1, 2, 3, or 4 arguments
+# generates completion reply with compgen
+__hnucomp ()
+{
+    local cur
+    _get_comp_words_by_ref cur
+    if [ $# -gt 2 ]; then
+        cur="$3"
+    fi
+    case "$cur" in
+        --*=)
+            COMPREPLY=()
+            ;;
+        *)
+            local IFS=$'\n'
+            COMPREPLY=( $(compgen -P "${2-}" \
+                        -W "$(__hnucomp_1 "${1-}" "${4-}")" \
+                        -- "$cur") )
+            ;;
+    esac
+} # __hnucomp ()
+
+_hnu ()
+{
+    local cur prev
 
     COMPREPLY=()
     _get_comp_words_by_ref cur prev
@@ -36,74 +133,37 @@ _hnu()
     # The hnu command
     if [[ $COMP_CWORD -eq 1 ]]; then
         if [[ "$cur" == --* ]] ; then
-            case "$cur" in
-                --conf=)
-                    COMPREPLY=( $(compgen -f -X "!*.php") )
-                    return
-                    ;;
-                *)
-                    COMPREPLY=( $(compgen -W "$global_opts" -- $cur) )
-                    ;;
-            esac
+            __hnucomp "--help --version --verbose --conf="
         else
-            COMPREPLY=( $(compgen -W "$commands" -- $cur) )
+            __hnucomp "init testserver runtests selftest server taskstart secretkey"
         fi
-        return
     fi
-
-    # prevents completion twice
-    case "$cur" in
-        --*=)
-            COMPREPLY=()
-            return
-            ;;
-    esac
 
     case "$prev" in
         secretkey)
-            case "$cur" in
-                --*)
-                    COMPREPLY=( $(compgen -W "--length= --help" -- $cur) )
-                    ;;
-            esac
+            [[ "$cur" == --* ]] && __hnucomp "--length= --help"
             ;;
         runtests)
-            case "$cur" in
-                --bootstrap=)
-                    COMPREPLY=( $(compgen -f -X "!*.php") )
-                    ;;
-                --coverage-html=)
-                    COMPREPLY=( $(compgen -d) )
-                    ;;
-                --*)
-                    COMPREPLY=( $(compgen -W "--coverage-html= --bootstrap= --help" -- $cur) )
-                    ;;
-            esac
+            [[ "$cur" == --* ]] && __hnucomp "--coverage-html= --bootstrap= --help"
             ;;
-        selftests)
-            case "$cur" in
-                --coverage-html=)
-                    COMPREPLY=( $(compgen -d) )
-                    ;;
-                --*)
-                    COMPREPLY=( $(compgen -W "--coverage-html= --help" -- $cur) )
-                    ;;
-            esac
+        selftest)
+            [[ "$cur" == --* ]] && __hnucomp "--coverage-html= --help"
             ;;
-        server)
+        server|--all|--server-id=|--wait=)
             if [[ "$cur" == --* ]]; then
-                COMPREPLY=( $(compgen -W "--all --server-id= --wait= --help" -- $cur) )
+                __hnucomp "--all --server-id= --wait= --help"
             else
-                COMPREPLY=( $(compgen -W "$server_commands" -- $cur) )
+                __hnucomp "start stop less list childstart"
             fi
             ;;
         list) # a server subcommand
-            [[ "$cur" == --* ]] && COMPREPLY=( $(compgen -W "--json --help" $cur) )
+            [[ "$cur" == --* ]] && __hnucomp "--json --help"
             ;;
         start) # a server subcommand
-            [[ "$cur" == --* ]] && COMPREPLY=( $(compgen -W "--children= --help" -- $cur) )
+            [[ "$cur" == --* ]] && __hnucomp "--children= --help"
             ;;
     esac
-}
+} # _hnu ()
 complete -o bashdefault -o default -o nospace -F _hnu hnu
+
 }
