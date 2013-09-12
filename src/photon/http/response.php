d6d8b1ace2fe4fca0e7f243f597588f8c76f8a12
William MARTIN <william.martin@power-lan.com>
2013-09-12 17:29:10 +0200

    Add Not Implemented (501) http answer

--- src/photon/http/response.php
+++ src/photon/http/response.php
@@ -31,72 +31,6 @@ use \photon\http\Response as Response;
 use \photon\mail\EMail as Mail;
 use \photon\template as template;
 
-class BadRequest extends Response
-{
-    public function __construct($request)
-    {
-        $content = 'The request could not be understood by the server due to malformed syntax.'
-            . "\n\n" . '400 - BadRequest';
-        parent::__construct($content, 'text/plain');
-        $this->status_code = 400;
-    }
-}
-
-class Forbidden extends Response
-{
-    public function __construct($request)
-    {
-        $content = 'You are not authorized to view this page. You do not have permission' . "\n"
-            . 'to view the requested directory or page using the credentials supplied.' . "\n\n"
-            . '403 - Forbidden';
-        parent::__construct($content, 'text/plain');
-        $this->status_code = 403;
-    }
-}
-
-class NotFound extends Response
-{
-    public function __construct($request)
-    {
-        $content = sprintf('The requested URL %s was not found on this server.' . "\n" .
-                           'Please check the URL and try again.' . "\n\n" . '404 - Not Found',
-                           str_replace(array('&',     '"',      '<',    '>'),
-                                       array('&amp;', '&quot;', '&lt;', '&gt;'),
-                                       $request->path));
-        parent::__construct($content, 'text/plain');
-        $this->status_code = 404;
-    }
-}
-
-class NotModified extends Response
-{
-    public function __construct($content='', $mimetype='text/html; charset=utf-8')
-    {
-        parent::__construct('', $mimetype);
-        $this->status_code = 304;
-    }
-}
-
-class NotSupported extends Response
-{
-    public function __construct($request, $allow=array('GET'))
-    {
-        $content = sprintf('HTTP method %s is not supported for the URL %s.' 
-                           . "\n" .
-                           'Supported methods are: %s.' . "\n" .
-                           '405 - Not Supported',
-                           htmlspecialchars($request->method), 
-                           str_replace(array('&',     '"',      '<',    '>'),
-                                       array('&amp;', '&quot;', '&lt;', '&gt;'),
-                                       $request->path),
-                           implode ($allow, ', ')
-                           );
-        parent::__construct($content, 'text/plain');
-        $this->headers['Allow'] = implode ($allow, ', ');
-        $this->status_code = 405;
-    }
-} 
-
 /**
  * A HTTP response doing a redirect.
  */
@@ -135,6 +69,82 @@ class FormRedirect extends Redirect
     }
 }
 
+class NotModified extends Response
+{
+    public function __construct($content='', $mimetype='text/html; charset=utf-8')
+    {
+        parent::__construct('', $mimetype);
+        $this->status_code = 304;
+    }
+}
+
+class BadRequest extends Response
+{
+    public function __construct($request)
+    {
+        $content = 'The request could not be understood by the server due to malformed syntax.'
+            . "\n\n" . '400 - BadRequest';
+        parent::__construct($content, 'text/plain');
+        $this->status_code = 400;
+    }
+}
+
+class Forbidden extends Response
+{
+    public function __construct($request)
+    {
+        $content = 'You are not authorized to view this page. You do not have permission' . "\n"
+            . 'to view the requested directory or page using the credentials supplied.' . "\n\n"
+            . '403 - Forbidden';
+        parent::__construct($content, 'text/plain');
+        $this->status_code = 403;
+    }
+}
+
+class NotFound extends Response
+{
+    public function __construct($request)
+    {
+        $content = sprintf('The requested URL %s was not found on this server.' . "\n" .
+                           'Please check the URL and try again.' . "\n\n" . '404 - Not Found',
+                           str_replace(array('&',     '"',      '<',    '>'),
+                                       array('&amp;', '&quot;', '&lt;', '&gt;'),
+                                       $request->path));
+        parent::__construct($content, 'text/plain');
+        $this->status_code = 404;
+    }
+}
+
+class NotSupported extends Response
+{
+    public function __construct($request, $allow=array('GET'))
+    {
+        $content = sprintf('HTTP method %s is not supported for the URL %s.' 
+                           . "\n" .
+                           'Supported methods are: %s.' . "\n" .
+                           '405 - Not Supported',
+                           htmlspecialchars($request->method), 
+                           str_replace(array('&',     '"',      '<',    '>'),
+                                       array('&amp;', '&quot;', '&lt;', '&gt;'),
+                                       $request->path),
+                           implode ($allow, ', ')
+                           );
+        parent::__construct($content, 'text/plain');
+        $this->headers['Allow'] = implode ($allow, ', ');
+        $this->status_code = 405;
+    }
+}
+
+class NotImplemented extends Response
+{
+    public function __construct($request)
+    {
+        $content = 'The server either does not recognize the request method, or it lacks the ability to fulfill the request' . "\n\n"
+                   . '501 - Not Implemented';
+        parent::__construct($content, 'text/plain');
+        $this->status_code = 501;
+    }
+}
 
 class RedirectToLogin extends Response
 {
