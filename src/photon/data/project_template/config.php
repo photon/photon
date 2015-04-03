ef093ec169879be8a365cf31ca52da86bd4ec72b
William MARTIN <wysman@gmail.com>
2015-04-03 09:45:23 +0200

    Rewrite a more simple project to start with photon

--- src/photon/data/project_template/config.php
+++ src/photon/data/project_template/config.php
@@ -1,36 +1,20 @@
 <?php
-/* -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/**
- * The project configuration file configures all the apps in your project.
- */
+
+// Set the initial log level
+\photon\log\Log::setLevel('INFO');
+
 return array(
-             // Once your work is completed, you must switch the flag
-             // to false as in case of errors in debug mode, a lot of
-             // data are printed on screen.
+    'debug' => true,
+    'secret_key' => 'A very long secret key to sign cookie',
 
-             // The main debug flag will set the logging to debug
-             // mode, but also the handling of errors.
-             'debug' => true,
-             'base_urls' => '/demo',
-             'urls' => include __DIR__.'/urls.php',
+    'base_urls' => '',
+    'urls' => include 'urls.php',
 
-             'secret_key' => '%%SECRET_KEY%%',
-             'admins' => array(array('1st Admin Name', 'admin1@example.com')),
+    'template_folders' => array(
+        __DIR__ .'/HelloWorld/templates',
+    ),
 
-             // List of installed applications
-             'installed_apps' => array('helloworld', 'photonchat'),
-
-             // The templates are compiled as .php files and are
-             // stored in the tmp folder.
-             'tmp_folder' => sys_get_temp_dir(),
-
-             // The template folders are where your templates are stored.
-             'template_folders' => array(__DIR__ . '/photonchat/templates'),
-             // List of installed tasks
-             'installed_tasks' => 
-             array('photonchat_server' => '\photonchat\task\Server'),
-             // And configuration for each task
-             'photon_task_photonchat_server' => 
-             array('m2_pub' => 'tcp://127.0.0.1:9996'),
-             );
-
+    'middleware_classes' => array(
+        'photon\middleware\Gzip',
+    ),
+);
