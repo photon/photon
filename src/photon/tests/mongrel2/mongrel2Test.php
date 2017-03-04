51d5d4cda5240314464ddc90fe802bb80e2f5683
William MARTIN <william.martin@power-lan.com>
2017-03-04 10:30:20 +0100

    Rewrite test mongrel2Test::testConnectionRecvGet

--- src/photon/tests/mongrel2/mongrel2Test.php
+++ src/photon/tests/mongrel2/mongrel2Test.php
@@ -26,6 +26,9 @@ namespace photon\tests\mongrel2\mongrel2Test;
 use \photon\test\TestCase;
 use \photon\mongrel2;
 
+/*
+ * DummyZMQSocket is used to mock the connect of mongrel2/Connection
+ */
 class DummyZMQSocket
 {
     public $payload = false;
@@ -92,11 +95,12 @@ class mongrel2Test extends TestCase
 
     public function testConnectionRecvGet()
     {
-        $this->markTestIncomplete('To rewrite');
+        $payload = '34f9ceee-cd52-4b7f-b197-88bf2f0ec378 6 /handlertest/foo 421:{"PATH":"/handlertest/foo","user-agent":"curl/7.19.7 (i486-pc-linux-gnu) libcurl/7.19.7 OpenSSL/0.9.8k zlib/1.2.3.3 libidn/1.15","host":"localhost:6767","accept":"*/*","content-type":"multipart/form-data; boundary=----------------------------b9069e918c9e","x-forwarded-for":"::1","content-length":"21894","METHOD":"GET","VERSION":"HTTP/1.1","URI":"/handlertest/foo?toto=titi","QUERY":"toto=titi","PATTERN":"/handlertest"},0:';
 
-        $socket = new DummyZMQSocket();
-        $socket->setNextRecv('34f9ceee-cd52-4b7f-b197-88bf2f0ec378 6 /handlertest/foo 421:{"PATH":"/handlertest/foo","user-agent":"curl/7.19.7 (i486-pc-linux-gnu) libcurl/7.19.7 OpenSSL/0.9.8k zlib/1.2.3.3 libidn/1.15","host":"localhost:6767","accept":"*/*","content-type":"multipart/form-data; boundary=----------------------------b9069e918c9e","x-forwarded-for":"::1","content-length":"21894","METHOD":"GET","VERSION":"HTTP/1.1","URI":"/handlertest/foo?toto=titi","QUERY":"toto=titi","PATTERN":"/handlertest"},0:');
-        $conn = new mongrel2\Connection($socket, null);
+        $conn = new mongrel2\Connection('tcp://127.0.0.1:12345');
+        $conn->pull_socket = new DummyZMQSocket();
+        $conn->pull_socket->setNextRecv($payload);
+
         $mess = $conn->recv();
         $this->assertEquals($mess->path, '/handlertest/foo');
     }
